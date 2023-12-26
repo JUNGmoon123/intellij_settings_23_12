4f9fb3343edc640d096ddd0c34b256525ee8148d
JUNGmoon123 <jungpro567@gmail.com>
2023-12-26 15:44:35 +0900

    테스트케이스와 assertThat 라이브러리 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome!");
-
+        System.out.println(new App().plus(10,20));
     }
 }
